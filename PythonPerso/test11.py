fc2f96001ccf847ad4bd6f744dfd0172c3b21005
SaffinALex <36388674+SaffinALex@users.noreply.github.com>
2018-02-12 23:16:20 +0100

    Add files via upload

--- PythonPerso/test11.py
+++ PythonPerso/test11.py
@@ -10,9 +10,11 @@ def placerBloc(nbr,x,y):
     nomBloc="bloc"+str(nbr)
     fichier = open(nomBloc,'r')
     lignes  = fichier.readlines()
+    
     if not(nomBloc=="bloc0"):
         Bloque_x.append(x)
         Bloque_y.append(y)
+
         
     for ligne in lignes:
         for i in range (0,16):
@@ -34,47 +36,84 @@ def afficheTerrain():
     global mapx
     global mapy
     
-    nom="map"+str(mapy)+"-"+str(mapx)
+    nom="map"+str(mapx)+"-"+str(mapy)
     fichier=open(nom,"r")
     lignes=fichier.readlines()
     j=0
 
     for ligne in lignes:
-        for i in range (0,19):
-            if i>0:
-                placerBloc(ligne[i],(i*32)-1,j)
-            else:
-                placerBloc(ligne[i],i,j)
-            
+        for i in range (0,20):
+            placerBloc(ligne[i],(i*32),j)
+
         j+=32
+        
+def ZeroBloque():
+    global Bloque_x
+    global Bloque_y
+    
+    while len(Bloque_x)>0:
+        del Bloque_x[len(Bloque_x)-1]
+        del Bloque_y[len(Bloque_y)-1]
 
 
-def Bloquer(val_x, val_y):
+def BloquerPerso(val_x, val_y):
     global Bloque_x
     global Bloque_y
     global MonPerso
     
     for i in range(0,len(Bloque_x)-1):
         
-        if(MonPerso[2]>=Bloque_x[i] and MonPerso[2]<=Bloque_x[i]+32):
-            if(MonPerso[3]>=Bloque_y[i] and MonPerso[3]<=Bloque_y[i]+32):
+        if(MonPerso[2]>=Bloque_x[i] and MonPerso[2]<=Bloque_x[i]+31):
+            if(MonPerso[3]>=Bloque_y[i] and MonPerso[3]<=Bloque_y[i]+31): #Coin Haut gauche
                 MonPerso[3]=val_y
                 MonPerso[2]=val_x
                 
-        if(MonPerso[2]+32>=Bloque_x[i] and MonPerso[2]+16<=Bloque_x[i]+32):
-            if(MonPerso[3]+32>=Bloque_y[i] and MonPerso[3]+16<=Bloque_y[i]+32):
+        if(MonPerso[2]+31>=Bloque_x[i] and MonPerso[2]+32<=Bloque_x[i]+31):
+            if(MonPerso[3]+31>=Bloque_y[i] and MonPerso[3]+31<=Bloque_y[i]+31): #Coin Haut Droite
                 MonPerso[3]=val_y
                 MonPerso[2]=val_x
 
-        if(MonPerso[2]+32>=Bloque_x[i] and MonPerso[2]+16<=Bloque_x[i]+32):
-            if(MonPerso[3]>=Bloque_y[i] and MonPerso[3]<=Bloque_y[i]+32):
+        if(MonPerso[2]+31>=Bloque_x[i] and MonPerso[2]+31<=Bloque_x[i]+31):
+            if(MonPerso[3]>=Bloque_y[i] and MonPerso[3]<=Bloque_y[i]+31): #Coin Bas Droite
                 MonPerso[3]=val_y
                 MonPerso[2]=val_x
 
-        if(MonPerso[2]>=Bloque_x[i] and MonPerso[2]<=Bloque_x[i]+32):
-            if(MonPerso[3]+16>=Bloque_y[i] and MonPerso[3]+16<=Bloque_y[i]+32):
+        if(MonPerso[2]>=Bloque_x[i] and MonPerso[2]<=Bloque_x[i]+31):
+            if(MonPerso[3]+31>=Bloque_y[i] and MonPerso[3]+31<=Bloque_y[i]+31): #Coin Bas gauche
                 MonPerso[3]=val_y
                 MonPerso[2]=val_x
+
+def BloquerEnnemi(val_x, val_y):
+    global Bloque_x
+    global Bloque_y
+    global MonEnnemi
+    global Deplacement
+    
+    for i in range(0,len(Bloque_x)-1):
+        
+        if(MonEnnemi[2]>=Bloque_x[i] and MonEnnemi[2]<=Bloque_x[i]+31):
+            if(MonEnnemi[3]>=Bloque_y[i] and MonEnnemi[3]<=Bloque_y[i]+31): #Coin Haut gauche
+                MonEnnemi[3]=val_y
+                MonEnnemi[2]=val_x
+                Deplacement=0
+                
+        if(MonEnnemi[2]+31>=Bloque_x[i] and MonEnnemi[2]+32<=Bloque_x[i]+31):
+            if(MonEnnemi[3]+31>=Bloque_y[i] and MonEnnemi[3]+31<=Bloque_y[i]+31): #Coin Haut Droite
+                MonEnnemi[3]=val_y
+                MonEnnemi[2]=val_x
+                Deplacement=0
+
+        if(MonEnnemi[2]+31>=Bloque_x[i] and MonEnnemi[2]+31<=Bloque_x[i]+31):
+            if(MonEnnemi[3]>=Bloque_y[i] and MonEnnemi[3]<=Bloque_y[i]+31): #Coin Bas Droite
+                MonEnnemi[3]=val_y
+                MonEnnemi[2]=val_x
+                Deplacement=0
+
+        if(MonEnnemi[2]>=Bloque_x[i] and MonEnnemi[2]<=Bloque_x[i]+31):
+            if(MonEnnemi[3]+31>=Bloque_y[i] and MonEnnemi[3]+31<=Bloque_y[i]+31): #Coin Bas gauche
+                MonEnnemi[3]=val_y
+                MonEnnemi[2]=val_x
+                Deplacement=0
            
 
 def mouvement_perso():
@@ -83,8 +122,10 @@ def mouvement_perso():
     global MonPerso
     global MonEnnemi
     global Deplacement
-    global Joueur
+    global Perso
     global Ennemi
+    global mapx
+    global mapy
 
     canvas.delete(Joueur)
     
@@ -93,29 +134,60 @@ def mouvement_perso():
     val_x=MonPerso[2]-MonPerso[0]
     val_y=MonPerso[3]-MonPerso[1]
 
-    Bloquer(val_x,val_y)
+    BloquerPerso(val_x,val_y)
     
-    Joueur=canvas.create_rectangle(MonPerso[2],MonPerso[3],MonPerso[2]+32,MonPerso[3]+32,fill="blue",outline="")
+    mouvePerso(MonPerso[2],MonPerso[3])
+    
+    if (MonPerso[2]+32>640):
+        mapx+=1
+        MonPerso[2]=0
+        ZeroBloque()
+        afficheTerrain()
+        mouvePerso(MonPerso[2],MonPerso[3])
+        
+    elif(MonPerso[2]<0):
+        mapx-=1
+        MonPerso[2]=640-32
+        ZeroBloque()
+        afficheTerrain()
+        mouvePerso(MonPerso[2],MonPerso[3])
+        
+    elif(MonPerso[3]<0):
+        mapy+=1
+        MonPerso[3]=640-32
+        ZeroBloque()
+        afficheTerrain()
+        mouvePerso(MonPerso[2],MonPerso[3])
+        
+    elif(MonPerso[3]+32>640):
+        mapy-=1
+        MonPerso[3]=0
+        ZeroBloque()
+        afficheTerrain()
+        mouvePerso(MonPerso[2],MonPerso[3])
+                
+        
 
-    if(Deplacement==0):
+    if(Deplacement<=0):
         chiffre=randint(0,4)
         MonEnnemi[0]=0
         MonEnnemi[1]=0
         
         if(chiffre==0):
-            MonEnnemi[0]+=2
+            MonEnnemi[0]+=4
         elif(chiffre==1):
-            MonEnnemi[0]-=2
+            MonEnnemi[0]-=4
         elif(chiffre==2):
-            MonEnnemi[1]+=2
+            MonEnnemi[1]+=4
         elif(chiffre==3):
-            MonEnnemi[1]-=2
+            MonEnnemi[1]-=4
         Deplacement=10
 
     MonEnnemi[2]+=MonEnnemi[0]
     MonEnnemi[3]+=MonEnnemi[1]
-    placerSkin(1,MonEnnemi[2],MonEnnemi[3])
-    print MonEnnemi[0]
+    BloquerEnnemi(MonEnnemi[2]-MonEnnemi[0],MonEnnemi[3]-MonEnnemi[1])
+    mouveEnnemi(1,MonEnnemi[2],MonEnnemi[3])
+
     Deplacement-=1
 
         
@@ -148,16 +220,22 @@ def stop(event):
     if(touche=="Right" or touche=="Left"):
         MonPerso[0]=0
         
-def effaceSkin(nbr,x,y):
+def effaceEnnemi():
     global Ennemi
     while len(Ennemi)>0:
         canvas.delete(Ennemi[len(Ennemi)-1])
         del Ennemi[len(Ennemi)-1]
+
+def effacePerso():
+    global Perso
+    while len(Perso)>0:
+        canvas.delete(Perso[len(Perso)-1])
+        del Perso[len(Perso)-1]
     
 
-def placerSkin(nbr,x,y):
+def mouveEnnemi(nbr,x,y):
     global Ennemi
-    effaceSkin(nbr,x,y)
+    effaceEnnemi()
     val_x=x
     val_y=y
     nomEnnemi="ennemi"+str(nbr)
@@ -165,7 +243,7 @@ def placerSkin(nbr,x,y):
     lignes  = fichier.readlines()
 
     for ligne in lignes:
-        for i in range (0,16):
+        for i in range (0,17):
             if ligne[i]=="1":
                 pos=canvas.create_rectangle(val_x,val_y,val_x+2,val_y+2,fill="green",outline="")
                 Ennemi.append(pos)
@@ -182,6 +260,32 @@ def placerSkin(nbr,x,y):
         val_y+=2
         val_x=x
 
+def mouvePerso(x,y):
+    global Perso
+    effacePerso()
+    val_x=x
+    val_y=y
+    fichier = open("Perso",'r')
+    lignes  = fichier.readlines()
+
+    for ligne in lignes:
+        for i in range (0,17):
+            if ligne[i]=="1":
+                pos=canvas.create_rectangle(val_x,val_y,val_x+2,val_y+2,fill="green",outline="")
+                Perso.append(pos)
+            elif ligne[i]=="2":
+                pos=canvas.create_rectangle(val_x,val_y,val_x+2,val_y+2,fill="black",outline="")
+                Perso.append(pos)
+            elif ligne[i]=="3":
+                pos=canvas.create_rectangle(val_x,val_y,val_x+2,val_y+2,fill="blue",outline="")
+                Perso.append(pos)
+                
+            val_x+=2
+
+
+        val_y+=2
+        val_x=x
+
         
 fenetre = Tk()
 
@@ -189,11 +293,12 @@ canvas = Canvas(fenetre, width=640, height=639, background="#F5F5DC")
 mapx=1
 mapy=1
 Deplacement=0
-MonPerso=[0,0,32,0]
-MonEnnemi=[0,0,100,150]
+MonPerso=[0,0,96,96]
+MonEnnemi=[0,0,64,64]
 Bloque_y=[]
 Bloque_x=[]
 Ennemi=[]
+Perso=[]
 Joueur=canvas.create_rectangle(MonPerso[2],MonPerso[3],MonPerso[2]+32,MonPerso[3]+32,fill="blue",outline="")
 afficheTerrain()
 mouvement_perso()
--- PythonPerso/time.py
+++ PythonPerso/time.py
@@ -2,7 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from Tkinter import *
+        
+fenetre = Tk()
 
+canvas = Canvas(fenetre, width=640, height=639, background="#F5F5DC")
+mapx=1
+mapy=1
+Deplacement=0
+MonPerso=[0,0,0,0]
+MonEnnemi=[0,0,100,150]
+Bloque_y=[]
+Bloque_x=[]
+Ennemi=[]
+Joueur=canvas.create_rectangle(MonPerso[2],MonPerso[3],MonPerso[2]+32,MonPerso[3]+32,fill="blue",outline="")
+canvas.pack()
+canvas.focus_set()
+
+
+        
+fenetre.mainloop()
 fichier = open("map1-1",'r')
 lignes  = fichier.readlines()
 for ligne in lignes:
